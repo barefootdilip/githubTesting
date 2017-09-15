cc5d33329984d8a55de019d5dbead42775759294
barefootdilip <diliptilonia4@gmail.com>
2017-09-15 20:52:52 +0530

    this is my first commit

--- githubTesting/ViewController.swift
+++ githubTesting/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ButtonCreated(_ sender: UIButton) {
+        print("Button clicked")
+    }
+    
 }
 
